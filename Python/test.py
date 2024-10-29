98c82b26f499726d4b4f587d3449f88747658ce5
Pawan Rajput <pawan.ggit.bca@gmail.com>
2024-10-29 11:41:30 +0530

    Added a test.py file

--- Python/test.py
+++ Python/test.py
@@ -1,2 +1,26 @@
+import sys
+print(sys.version)
 print("Testing first script")
+
+if 5<6:
+    print("True statement")
+    print("False statement")
+    # Varibles in python
+x=5
+y=10
+z="He who remains"
+print(x,y,z)
+#type of varible 
+a=int(3)   # It will return the int value 3.
+b=str("he who remains")   # It will return the string as he who remains 
+c=float(9) #It will return the float value as 9.0
+print(a,b,c)
+
+#Type function in python
+m=4
+n="he who remains"
+o=0.0
+print(type(m))  #It will return the int class as it belongs to int class
+print(type(n))  #It will return the string class
+print(type(o))   #It will return the float class as its float value.
 
