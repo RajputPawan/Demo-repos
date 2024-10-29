a7a384ae44b41fe54a3cff2817a69343bb31fea8
Pawan Rajput <pawan.ggit.bca@gmail.com>
2024-10-29 18:28:57 +0530

    added some lines in funcheck.py

--- Python/funcheck.py
+++ Python/funcheck.py
@@ -6,3 +6,21 @@ def myfun():
 
 print("Python is :", x)
 myfun()
+
+
+
+#String slicing
+b="Old is gold"
+a="Hello, World!"
+print(a[7]) #Slicing it will print W as we start counting form 0
+print(b[0:2])
+print(b[:-1])
+
+a="""\nMultiline 
+Test if it works
+souds cool\n"""
+print(a)
+print(len(a))
+#loop
+for x in "Papaya":
+      print(x)
